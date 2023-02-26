09a4bd9e0b7ac2dbc448b8e5453617ff3471c5b9
neo-the-one786 <119244854+neo-the-one786@users.noreply.github.com>
2023-02-26 10:47:52 +0530

    Create BubbleSort.java

---
+++ Arrays/BubbleSort.java
@@ -0,0 +1,31 @@
+
+public class Solution {  
+
+  	public static void bubbleSort(int[] arr){
+    	//Your code goes here
+		int n = arr.length;
+		
+		for(int i = 0;i<n-1;i++)
+		{
+			for(int j = 0;j<n-1;j++)
+			{
+				if(arr[j]>arr[j+1])
+				{
+					int temp = arr[j];
+					arr[j]=arr[j+1];
+					arr[j+1]= temp;
+				}
+			}
+		}
+    }
+     
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arr[] = {2,13,4,1,3,6,28};
+		bubbleSort(arr);
+
+	}
+}
+Footer
+© 2023 GitHub, Inc
