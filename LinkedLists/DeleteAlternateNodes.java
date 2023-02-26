ba4d476fdb25c881b415cf473d9b4b323c651635
neo-the-one786 <119244854+neo-the-one786@users.noreply.github.com>
2023-02-26 11:40:56 +0530

    Update DeleteAlternateNodes.java

--- LinkedLists/DeleteAlternateNodes.java
+++ LinkedLists/DeleteAlternateNodes.java
@@ -1,23 +1,4 @@
 
-    class Node<T> {
-        public T data;
-        public Node<T> next;
-
-        public Node(T data) {
-            this.setData(data);
-            this.next = null;
-        }
-
-        public T getData() {
-            return data;
-        }
-
-        public void setData(T data) {
-            this.data = data;
-        }
-
-    }
-*/
 
 public class Solution {
 
